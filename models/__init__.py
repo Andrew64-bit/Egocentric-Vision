8e7349b10ca1e76f8295939b789c54a47200270f
Andrew64-bit <80745782+Andrew64-bit@users.noreply.github.com>
2024-06-02 20:47:27 +0200

    Added import from models init  (VAE)

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from models.VideoModel import VideoModel
 from models.I3D import I3D
 from models.FinalClassifier import MLP,MLPWithDropout, LSTMClassifier, TransformerClassifier, TRNClassifier
+from models.VAE import VAE
