89ccaf3583b714899c4589e771e64c9c16d44f8d
Francesco <s270989@studenti.polito.it>
2024-06-14 22:46:42 +0200

    clean code

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -9,36 +9,6 @@ import math
 
 
 
-class MLP(nn.Module):
-    def __init__(self, clip_feature_dim, num_class):
-        super(MLP, self).__init__()
-        self.num_class = num_class
-        self.clip_feature_dim = clip_feature_dim
-        self.classifier = self.fc_fusion()
-        
-    def fc_fusion(self):
-        num_bottleneck = 512
-        classifier = nn.Sequential(
-                nn.ReLU(),
-                nn.Linear(self.clip_feature_dim, num_bottleneck),
-                nn.ReLU(),
-                nn.Linear(num_bottleneck,self.num_class),
-                )
-        return classifier
-    def forward(self, input):
-        output = self.classifier(input)
-        return output
-'''    
-    def __init__(self, num_classes):
-        super().__init__()
-        """
-        [TODO]: the classifier should be implemented by the students and different variations of it can be tested
-        in order to understand which is the most performing one """
-
-    def forward(self, x):
-        return self.classifier(x), {}'''
-
-
 class MLPWithDropout(nn.Module):
     def __init__(self, clip_feature_dim, num_class, num_bottleneck = 512, num_bottleneck1 = 256, dropout = 0.5):
         super(MLPWithDropout, self).__init__()
